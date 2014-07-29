03fb82719365ac0a9ac66176975ed24052dd237d
Samantha Zhang <i@samanthaz.me>
2014-07-28 23:43:56 -0700

    Console Log Blogs

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,24 @@
-Parse.initialize("MuLlfET9KdSdwJ70aol03zHmu5bNTGprdu5jZpec", "NslqpkwkAsRP3gxw5pSlf8gw9PJhKqNW6UbikTK3");
+$(function() {
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
+	Parse.$ = jQuery;
+
+	Parse.initialize("MuLlfET9KdSdwJ70aol03zHmu5bNTGprdu5jZpec", "NslqpkwkAsRP3gxw5pSlf8gw9PJhKqNW6UbikTK3");
+
+	var Blog = Parse.Object.extend("Blog"),
+
+		Blogs = Parse.Collection.extend({
+			model: Blog
+		}),
+
+		blogs = new Blogs();
+
+	blogs.fetch({
+		success: function(blogs) {
+			console.log(blogs);
+		},
+		error: function(blogs, error) {
+			console.log(error);
+		}
+	});
+
 });
