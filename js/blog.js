8d8f526c4e23cf29e2d514fc4954ad5b05dd11dd
Samantha Zhang <i@samanthaz.me>
2014-07-29 22:02:18 -0700

    Getting and Rendering Data from Database

--- js/blog.js
+++ js/blog.js
@@ -2,19 +2,55 @@ $(function() {
 
 	Parse.$ = jQuery;
 
-	Parse.initialize("MuLlfET9KdSdwJ70aol03zHmu5bNTGprdu5jZpec", "NslqpkwkAsRP3gxw5pSlf8gw9PJhKqNW6UbikTK3");
+	Parse.initialize(
+		"MuLlfET9KdSdwJ70aol03zHmu5bNTGprdu5jZpec", 
+		"NslqpkwkAsRP3gxw5pSlf8gw9PJhKqNW6UbikTK3");
 
-	var Blog = Parse.Object.extend("Blog"),
+	var $container = $('.blogs-container'),
+
+		Blog = Parse.Object.extend("Blog"),
+
+		BlogView = Parse.View.extend({
+
+			className: 'blog-post',
+
+			template: _.template(''
+				+ '<h2 class="blog-post-title"><%= title %></h2>'
+	            + '<p class="blog-post-meta"><%= createdAt %> by <a href="#"><%= author %></a></p>'
+	            + '<div><%= content %></div>'),
+
+			render: function(){
+				var attributes = this.model.toJSON();
+				this.$el.html(this.template(attributes));		
+			}
+
+		}),
 
 		Blogs = Parse.Collection.extend({
 			model: Blog
+		}),
+
+		BlogsView = Parse.View.extend({
+			
+			renderOne: function(blog){
+				var blogView = new BlogView({ model: blog });
+				blogView.render();
+				this.$el.append(blogView.el);
+			},
+
+			render: function(){ 
+				this.collection.forEach(this.renderOne, this);
+			},
+
 		}),
 
 		blogs = new Blogs();
 
 	blogs.fetch({
 		success: function(blogs) {
-			console.log(blogs);
+			var blogsView = new BlogsView({ collection: blogs });
+			blogsView.render();
+			$container.html(blogsView.el);
 		},
 		error: function(blogs, error) {
 			console.log(error);
